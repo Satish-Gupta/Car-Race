dd996fb48f3b08c992440bbf7f1c477fd7c4cad0
Satish <satis.kumar.gupta@gmail.com>
2014-11-24 04:37:58 +0545

    collision detection

--- js/car-game.js
+++ js/car-game.js
@@ -3,6 +3,7 @@
  */
 
 var gameRefreshRate = 20;
+var welcomeScreen = document.getElementById('welcome-screen');
 var opponentCars = [];
 
 var trackLength = 10000;
@@ -15,7 +16,7 @@ var roadContainer = document.getElementById('main-container');
 roadContainer.style.height = visibleTrack + "px";
 roadContainer.style.width = trackWidth + "px";
 
-var road = document.getElementById('road-wrapper');
+var road = document.getElementById('road');
 road.style.width = trackWidth + "px";          // width of track
 road.style.height = trackLength + "px";          // length of track
 
@@ -24,12 +25,14 @@ var trackTopPos = -(trackLength - visibleTrack);
 
 var carWidth = 50;
 var carLength = 60;
-var carColour = "yellow";
+var carImages = ['car1', 'car2', 'car3'];
+var explosionImages = ['explosion-top', 'explosion-bottom'];
+
 var interCarGap = 4 * carLength;
 var playerCarTailPos = 20;        // distance between the the tail position of car and the bottom of the visible track
-//var carPositionsX = [20, (trackWidth / 2) - (carWidth / 2), 220];     // possible car x-positions
+
 var carPositionsX = [];
-for(var i = 0,temp = laneWidth / 2 - carWidth / 2;i < trackWidth / laneWidth; i++) {
+for (var i = 0, temp = laneWidth / 2 - carWidth / 2; i < trackWidth / laneWidth; i++) {
     carPositionsX.push(temp);
     temp += laneWidth;
 }
@@ -39,7 +42,7 @@ var playerCarPosY = trackLength - (carLength + playerCarTailPos);
 var playerCarPosX = (trackWidth / 2) - (carWidth / 2);
 
 var player_car = new Car();
-player_car.createCar(playerCarPosX, playerCarPosY);
+player_car.createCar(playerCarPosX, playerCarPosY, 'playerCar1');
 road.appendChild(player_car.element);
 
 function Car() {
@@ -48,11 +51,11 @@ function Car() {
     this.element;
     var that = this;
 
-    this.createCar = function (xCord, yCord) {
+    this.createCar = function (xCord, yCord, carImage) {
 
         that.element = document.createElement('div');
         that.element.className = "car";
-        that.element.style.background = carColour;
+        that.element.style.background = 'url("images/' + carImage + '.png")';
         that.element.style.width = carWidth + "px";
         that.element.style.height = carLength + "px";
         that.element.style.position = "absolute";
@@ -65,8 +68,6 @@ function Car() {
 
     }
 }
-
-
 
 function gameloop() {
 
@@ -93,7 +94,7 @@ function gameloop() {
 
         xCord = carPositionsX[carPositonsIndex];
 
-        opponent.createCar(xCord, yCord);
+        opponent.createCar(xCord, yCord, carImages[Math.round(Math.random() * 2)]);
         opponentCars.push(opponent);
         road.appendChild(opponent.element);
 
@@ -103,17 +104,34 @@ function gameloop() {
 
     }
 
-    // remove cars past visible track
+
     for (var i = 0; i < opponentCars.length; i++) {
-        if (opponentCars[i] != undefined) {
-            if (Math.abs(trackTopPos) <= (opponentCars[i].y - visibleTrack)) {
-                road.removeChild(opponentCars[i].element);
+
+        currCar = opponentCars[i];
+        if (currCar != undefined) {
+            // remove cars past visible track
+            if (Math.abs(trackTopPos) <= (currCar.y - visibleTrack)) {
+                road.removeChild(currCar.element);
                 delete opponentCars[i];
+            }
+
+            // detect collison and stop the car and disable car movement(left right)
+            if (currCar.x == player_car.x && (currCar.y + carLength) >= player_car.y && currCar.y <= (player_car.y + carLength)) {
+
+                carSpeed = 0;
+                currCar.element.style.background = 'url("images/' + explosionImages[0] + '.png")';
+                player_car.element.style.background = 'url("images/' + explosionImages[1] + '.png")';
+                document.removeEventListener("keydown", keyEventListener);
+
+                welcomeScreen.innerHTML = "<h1>Game Over</h1>";
+                welcomeScreen.style.top = (player_car.y - 300) + "px";
+
             }
         }
     }
 
     function moveOpponent() {
+
         trackTopPos = trackTopPos + carSpeed;
         road.style.top = trackTopPos + "px";
     }
@@ -123,23 +141,35 @@ function gameloop() {
 
         moveOpponent();
 
+    } else {
+
+        welcomeScreen.innerHTML = "<h1>Congratulation! you won</h1>";
+        welcomeScreen.style.top = (visibleTrack - 300) + "px";
     }
 //    counter += 1;
 }
 
-setInterval(gameloop, gameRefreshRate);
+welcomeScreen.onclick = function () {
+    setInterval(gameloop, gameRefreshRate);
+    welcomeScreen.style.background = "none";
+    welcomeScreen.innerHTML = "";
+};
 
-document.addEventListener("keydown",function(event) {
+function keyEventListener(event) {
+
     var left = player_car.x - laneWidth;
     var right = player_car.x + laneWidth;
 
+    if (event.keyCode == "37" && left >= (laneWidth / 2 - carWidth / 2)) {
 
-    if(event.keyCode == "37" && left >= (laneWidth / 2 - carWidth / 2)) {
         player_car.x = left;
         player_car.element.style.left = left + "px";
-    } else if (event.keyCode =="39" && right < trackWidth - (laneWidth/2)) {
+
+    } else if (event.keyCode == "39" && right < trackWidth - (laneWidth / 2)) {
+
         player_car.x = right;
         player_car.element.style.left = right + "px";
 
     }
-});
+}
+document.addEventListener("keydown", keyEventListener);
